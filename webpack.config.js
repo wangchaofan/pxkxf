c0a76efed6c9014b1c17cc7e8ee438e276ba8d1d
wangchaofan <514610423@qq.com>
2016-12-21 01:18:09 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+'use strict';
+const webpack = require('webpack')
+const path = require('path')
+const fs = require('fs')
+const glob = require("glob")
+
+const debug = process.argv.indexOf('-d') !== -1
+
+let plugins = [
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: debug,
+        },
+        output: {
+            comments: false,
+        },
+    })
+  ]
+
+let globmaths =  glob.sync("./src/pages/*.js",{
+  nodir:true,
+})
+
+let entry = globmaths.reduce((obj,file)=>{
+  let filename = path.basename(file).split(".")[0]
+  obj[filename] = ["babel-polyfill",file]
+  return obj
+},{})
+
+module.exports = {
+    entry:entry,
+    output: {
+        path: `./lib`,
+        filename: "[name].js",
+    },
+    module: {
+      loaders: [
+        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      ]
+    },
+    plugins: plugins,
+};
