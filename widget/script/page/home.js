06ba7347a57e8359158515f90b803ef1116e5504
wangchaofan <514610423@qq.com>
2016-12-23 21:15:34 +0800

    fix

---
+++ widget/script/page/home.js
@@ -0,0 +1,12 @@
+/**
+ * Created by chaofanw on 2016/12/23.
+ */
+$(function() {
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    autoplay: 3000,
+
+    // 如果需要分页器
+    pagination: '.swiper-pagination'
+  })
+})
--- widget/script/page/index.js
+++ widget/script/page/index.js
@@ -1,49 +1,141 @@
-$(function() {
-	var mySwiper = new Swiper('.swiper-container', {
-		loop: true,
-		autoplay: 3000,
-
-		// 如果需要分页器
-		pagination: '.swiper-pagination'
-	})
-})
-
 apiready = function() {
-	// api.openFrameGroup({
-	// 	name: 'group1',
-	// 	background: '#fff',
-	// 	scrollEnabled: false,
-	// 	rect: {
-	// 		x: 0,
-	// 		y: 0,
-	// 		w: 'auto',
-	// 		h: 'auto'
-	// 	},
-	// 	index: 0,
-	// 	frames: [{
-	// 		name: 'consult',
-	// 		url: 'html/consult.html',
-	// 		bgColor: '#fff'
-	// 	}, {
-	// 		name: 'order',
-	// 		url: 'html/order.html',
-	// 		bgColor: '#fff'
-	// 	}, {
-	// 		name: 'usercenter',
-	// 		url: 'html/usercenter.html',
-	// 		bgColor: '#fff'
-	// 	}]
-	// }, function(ret, err) {
-	// 	var index = ret.index;
-	// })
-
-	var vm = new Vue({
-		el: 'body',
-		data: {
-
+	var nvTabBarCommonTitle = {
+		text: '首页',
+		size: 13.0,
+		normal: '#333',
+		selected: '#ec3c41',
+		marginB: 6.0
+	}
+	var nvTabBarWidth = api.winWidth / 5.0
+	var NVTabBar = api.require('NVTabBar')
+	NVTabBar.open({
+		styles: {
+			bg: '#fdfdfd',
+			h: 52,
+			dividingLine: {
+				width: 0.5,
+				color: '#ddd'
+			},
+			badge: {
+				bgColor: '#ff0',
+				numColor: '#fff',
+				size: 6.0
+			}
 		},
-		methods: {
+		items: [{
+			w: nvTabBarWidth,
+			bg: {
+				marginB: 0
+			},
+			iconRect: {
+				w: 21.0,
+				h: 18.0
+			},
+			icon: {
+				normal: 'widget://image/nvtab/icon_home.png',
+				selected: 'widget://image/nvtab/icon_home_selected.png'
+			},
+			title: _.assign({}, nvTabBarCommonTitle, { text: '首页'})
+		}, {
+			w: nvTabBarWidth,
+			bg: {
+				marginB: 0
+			},
+			iconRect: {
+				w: 18,
+				h: 18
+			},
+			icon: {
+				normal: 'widget://image/nvtab/icon_order.png',
+				selected: 'widget://image/nvtab/icon_order_selected.png'
+			},
+			title: _.assign({}, nvTabBarCommonTitle, { text: '订单'})
+		}, {
+			w: nvTabBarWidth,
+			bg: {
+				marginB: 12
+			},
+			iconRect: {
+				w: 40,
+				h: 40
+			},
+			icon: {
+				normal: 'widget://image/nvtab/icon_add.png',
+				highlight: 'widget://image/nvtab/icon_add.png',
+				selected: 'widget://image/nvtab/icon_add.png'
+			},
+			title: _.assign({}, nvTabBarCommonTitle, { text: '发布', selected: '#333'})
+		}, {
+			w: api.winWidth / 5.0,
+			bg: {
+				marginB: -2
+			},
+			iconRect: {
+				w: 24.0,
+				h: 24.0
+			},
+			icon: {
+				normal: 'widget://image/nvtab/icon_zixun.png',
+				selected: 'widget://image/nvtab/icon_zixun_selected.png'
+			},
+			title: _.assign({}, nvTabBarCommonTitle, { text: '咨询' })
+		}, {
+			w: nvTabBarWidth,
+			bg: {
+				marginB: 0
+			},
+			iconRect: {
+				w: 19.0,
+				h: 19.0
+			},
+			icon: {
+				normal: 'widget://image/nvtab/icon_user.png',
+				selected: 'widget://image/nvtab/icon_user_selected.png'
+			},
+			title: _.assign({}, nvTabBarCommonTitle, { text: '我的' })
+		}],
+		selectedIndex: 0
+	}, function(ret, err) {
+		if (ret && ret.eventType === 'click') {
+			if (ret.index < 2) {
+				switchFrame(ret.index)
+			} else if (ret.index === 2) {
 
+			} else {
+				switchFrame(ret.index - 1)
+			}
 		}
 	})
+
+	api.openFrameGroup({
+		name: 'group',
+		background: '#fff',
+		scrollEnabled: false,
+		rect: {
+			x: 0,
+			y: 0,
+			w: 'auto',
+			h: 'auto'
+		},
+		index: 0,
+		frames: [{
+			name: 'home',
+			url: 'html/home.html'
+		}, {
+			name: 'order',
+			url: 'html/order.html'
+		}, {
+			name: 'consult',
+			url: 'html/consult.html'
+		}, {
+			name: 'usercenter',
+			url: 'html/usercenter.html'
+		}]
+	}, function(ret, err) {
+		var index = ret.index;
+	})
+
+	function switchFrame(index) {
+		api.setFrameGroupIndex({ name: 'group', index: index })
+	}
 }
