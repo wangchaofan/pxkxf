05cd89ce5d93ccbec07f2a7ecea711b9e4276927
wangchaofan <514610423@qq.com>
2016-12-23 10:45:13 +0800

    fix

---
+++ widget/script/page/index.js
@@ -0,0 +1,49 @@
+$(function() {
+	var mySwiper = new Swiper('.swiper-container', {
+		loop: true,
+		autoplay: 3000,
+
+		// 如果需要分页器
+		pagination: '.swiper-pagination'
+	})
+})
+
+apiready = function() {
+	api.openFrameGroup({
+		name: 'group1',
+		background: '#fff',
+		scrollEnabled: false,
+		rect: {
+			x: 0,
+			y: 0,
+			w: 'auto',
+			h: 'auto'
+		},
+		index: 0,
+		frames: [{
+			name: 'consult',
+			url: 'html/consult.html',
+			bgColor: '#fff'
+		}, {
+			name: 'order',
+			url: 'html/order.html',
+			bgColor: '#fff'
+		}, {
+			name: 'usercenter',
+			url: 'html/usercenter.html',
+			bgColor: '#fff'
+		}]
+	}, function(ret, err) {
+		var index = ret.index;
+	})
+
+	var vm = new Vue({
+		el: 'body',
+		data: {
+
+		},
+		methods: {
+
+		}
+	})
+}
