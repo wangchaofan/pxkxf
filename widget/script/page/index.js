60d2c80e763f1ba96c2821a46aaff43f26bef7ce
wangchaofan <514610423@qq.com>
2016-12-23 10:56:40 +0800

    fix

--- widget/script/page/index.js
+++ widget/script/page/index.js
@@ -9,33 +9,33 @@ $(function() {
 })
 
 apiready = function() {
-	api.openFrameGroup({
-		name: 'group1',
-		background: '#fff',
-		scrollEnabled: false,
-		rect: {
-			x: 0,
-			y: 0,
-			w: 'auto',
-			h: 'auto'
-		},
-		index: 0,
-		frames: [{
-			name: 'consult',
-			url: 'html/consult.html',
-			bgColor: '#fff'
-		}, {
-			name: 'order',
-			url: 'html/order.html',
-			bgColor: '#fff'
-		}, {
-			name: 'usercenter',
-			url: 'html/usercenter.html',
-			bgColor: '#fff'
-		}]
-	}, function(ret, err) {
-		var index = ret.index;
-	})
+	// api.openFrameGroup({
+	// 	name: 'group1',
+	// 	background: '#fff',
+	// 	scrollEnabled: false,
+	// 	rect: {
+	// 		x: 0,
+	// 		y: 0,
+	// 		w: 'auto',
+	// 		h: 'auto'
+	// 	},
+	// 	index: 0,
+	// 	frames: [{
+	// 		name: 'consult',
+	// 		url: 'html/consult.html',
+	// 		bgColor: '#fff'
+	// 	}, {
+	// 		name: 'order',
+	// 		url: 'html/order.html',
+	// 		bgColor: '#fff'
+	// 	}, {
+	// 		name: 'usercenter',
+	// 		url: 'html/usercenter.html',
+	// 		bgColor: '#fff'
+	// 	}]
+	// }, function(ret, err) {
+	// 	var index = ret.index;
+	// })
 
 	var vm = new Vue({
 		el: 'body',
