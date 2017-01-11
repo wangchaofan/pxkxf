4db996df109a214bc4eb91a2dd92fd512a36a06f
wangchaofan <514610423@qq.com>
2017-01-11 17:35:43 +0800

    fix

--- widget/script/page/professor_question.js
+++ widget/script/page/professor_question.js
@@ -4,13 +4,55 @@
 function initPage() {
   var vm = new Vue({
     el: '.wrapper',
+    created: function() {
+      this.getQuestions()
+      this.getProfessors()
+    },
     data: function() {
       return {
-
+        hotQuestions: [],
+        newQuestions: [],
+        bestProfessors: [],
+        freeProfessors: []
       }
     },
     methods: {
-
+      viewProfessorDetail: function(pf) {
+        console.log(pf)
+      },
+      viewQuestionDetail: function(qt) {
+        console.log(qt)
+      },
+      getQuestions: function() {
+        var self = this
+        $.ajax({
+          url: BaseService.apiUrl + 'getTW',
+          data: {
+            userid: self.userid
+          }
+        }).then(function(res) {
+          if (res.key === 'true') {
+            var list = ParseJson(res.data)
+            self.hotQuestions = list.slice(0, 2)
+            self.newQuestions = list.slice(0, 2)
+          }
+        })
+      },
+      getProfessors: function() {
+        var self = this
+        $.ajax({
+          url: BaseService.apiUrl + 'getExpert',
+          data: {
+            userid: self.userid
+          }
+        }).then(function(res) {
+          if (res.key === 'true') {
+            var list = ParseJson(res.data)
+            self.freeProfessors = list.slice(0, 2)
+            self.bestProfessors = list.slice(0, 2)
+          }
+        })
+      }
     }
   })
 }
