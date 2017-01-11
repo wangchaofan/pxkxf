6540df981c47e06976eeb5fbe4e551c834360e8e
wangchaofan <514610423@qq.com>
2017-01-12 02:47:32 +0800

    fix

--- widget/script/page/add_question.js
+++ widget/script/page/add_question.js
@@ -6,14 +6,35 @@ function initPage() {
     el: '.wrapper',
     data: function() {
       return {
-        twtitle: '',
-        twcontent: '',
-        imgarr: []
+        question: {
+          twuserid: Helper.getUserId,
+          title: '',
+          content: '',
+          imgarr: []
+        },
+        images: []
       }
     },
     methods: {
+      onSelectImage: function(e) {
+        var file = e.target.files[0]
+        var self = this
+        if (file) {
+          Helper.imagePreview(file).then(function(img) { self.images.push(img) })
+        }
+      },
+      deleteImage: function(index) {
+        this.images.splice(index, 1)
+      },
       onSubmit: function() {
-
+        var self = this
+        this.question.imgarr = _.map(this.images, Helper.transformImageData)
+        $.ajax({
+          url: BaseService.apiUrl + 'addtwinfo',
+          data: this.question 
+        }).then(function(res) {
+          console.log(res)
+        })
       }
     }
   })
