370cbe77aac2cee12ab2552a0e3a50cadfc8dbb0
wangchaofan <514610423@qq.com>
2017-01-13 15:21:54 +0800

    fix

--- widget/script/page/withdraw.js
+++ widget/script/page/withdraw.js
@@ -6,11 +6,60 @@ function initPage() {
     el: '.wrapper',
     data: function() {
       return {
-
+        userid: Helper.getUserId(),
+        money: '',
+        accountName: '',
+        waccount: '',
+        bankcardKHH: '',
+        type: '银行卡号',
+        wlabel: '',
+        wplaceholder: '',
+        submiting: false
+      }
+    },
+    computed: {
+      canSubmit: function() {
+        var k = !!this.userid && this.money > 0 && !!this.accountName && !!this.waccount && !!this.type && !this.submiting
+        return this.wtype === '银行卡号' ? (k && !!this.bankcardKHH) : k
+      },
+      wlabel: function() {
+        if (this.type === '银行卡号') {
+          return '银行卡号'
+        }
+        return this.type + '账号'
+      },
+      wplaceholder: function() {
+        if (this.type === '银行卡号') {
+          return '请输入银行卡卡号'
+        } else if (this.type === '微信') {
+          return '请输入微信账号'
+        }
+        return '请输入支付宝账号'
       }
     },
     methods: {
-
+      onSubmit: function () {
+        if (!this.canSubmit) return
+        var self = this
+        this.submiting = true
+        $.ajax({
+          url: BaseService.apiUrl + 'yuetx',
+          data: _.pick(self, ['userid', 'money', 'accountName', 'waccount', 'bankcardKHH', 'type'])
+        }).then(function (res) {
+          if (res.key === 'true') {
+            api.toast({
+                msg: '提交成功'
+            })
+            api.closeWin()
+          } else {
+            api.toast({
+                msg: res.mage
+            });
+          }
+        }).always(function() {
+          self.submiting = false
+        })
+      }
     }
   })
 }
