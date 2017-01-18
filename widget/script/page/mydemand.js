258e011e09d4e32343269e6cbc81942a20f2a476
wangchaofan <514610423@qq.com>
2017-01-18 15:15:50 +0800

    fix

--- widget/script/page/mydemand.js
+++ widget/script/page/mydemand.js
@@ -19,7 +19,7 @@ var ListItem = {
             '</li>',
   props: ['myData'],
   data: function() {
-    return []
+    return {}
   },
   computed: {
     avatar: function() {
@@ -75,6 +75,73 @@ var ListItem = {
     }
   }
 }
+
+var InviteItem = {
+  template: '<li class="supply-list-item" @click="viewDetail">' +
+            '  <div class="supply-list-item__left">' +
+            '    <img :src="avatar" alt="">' +
+            '  </div>' +
+            '  <div class="supply-list-item__right">' +
+            '    <div class="supply-list-item__param">' +
+            '      需求名称：<span class="text-black">{{demand.demandTitle}}</span>' +
+            '    </div>' +
+            '    <div class="supply-list-item__param">' +
+            '      需求价格：<strong class="text-warning">{{demand.dmoney}}</strong>元' +
+            '    </div>' +
+            '    <div class="supply-list-item__param">' +
+            '      需求状态：<span class="supply-status">{{getStateText(demand.orderState)}}</span>' +
+            '    </div>' +
+            '    <div class="supply-item-desc">描述：{{demand.ddetails}}</div>' +
+            '  </div>' +
+            '</li>',
+  props: ['myData'],
+  data: function() {
+    return {}
+  },
+  computed: {
+    demand: function() {
+      return this.myData.DemandOrder[0]
+    },
+    avatar: function() {
+      return this.myData ? this.myData.DemandOrder[0].Usermodel[0].pheadimgUrl : ''
+    }
+  },
+  methods: {
+    viewDetail: function() {
+      var self = this
+      api.openWin({
+        name: 'demand_detail',
+        url: 'widget://html/demand_detail.html',
+        pageParam: {
+          id: this.myData.demandorderId
+        }
+      })
+    },
+    getStateText: function(state) {
+      switch(state) {
+        case 1: 
+          return '待支付'
+        case 2:
+          return '审核中'
+        case 3: 
+          return '发布中'
+        case 4: 
+          return '进行中'
+        case 5: 
+          return '供应完成'
+        case 6: 
+          return '待评价'
+        case 7:
+          return '订单完成'
+        case 8: 
+          return '已关闭'
+        default:
+          return '已删除'
+      }
+    }
+  }
+}
+
 function initPage() {
   var vm = new Vue({
     el: '.wrapper',
@@ -82,7 +149,8 @@ function initPage() {
       this.getData()
     },
     components: {
-      'list-item': ListItem
+      'list-item': ListItem,
+      'invite-item': InviteItem
     },
     data: function() {
       return {
@@ -125,7 +193,7 @@ function initPage() {
             data: { userid: Helper.getUserId() }
           }).then(function(res) {
             self.inviteList = ParseJson(res.data)
-            console.log(self.inviteList)
+            console.log(ParseJson(res.data))
           })
         }
       }
